9d5848ec984d83ad0e82a4e1d20d04588a1ed902
VitorScallen77 <sscallen199@gmail.com>
2024-10-30 20:58:10 -0300

    include project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com o iPhone
+        Smartphone iphone = new Iphone("123456789", "iPhone 13", "111222333444555", 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Teste com o Nokia
+        Smartphone nokia = new Nokia("987654321", "Nokia Lumia", "555444333222111", 64);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Telegram");
+    }
+}
